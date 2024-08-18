d69ed4bb4e4de975729d1af695e8854e06e354bb
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-08-19 00:20:52 +0500

    Add files via upload

--- Assets/project.js
+++ Assets/project.js
@@ -1,34 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    AskForCookieConsent();
-    // Get the hash from the URL
-    const hash = window.location.hash.substring(1);
+    document.addEventListener("DOMContentLoaded", function() {
+        AskForCookieConsent();
+        // Get the hash from the URL
+        const hash = window.location.hash.substring(1);
+        const categoryFilter = document.getElementById('categoryFilter');
+        if (hash) {
+            // Set the dropdown to the hash value
+            categoryFilter.value = hash;
+            filterProducts();
+        }
+    });
+        
+        
+    const products = [
+        { imageURL: "https://project.mbktechstudio.com/Assets/Images/CTMCpp.png",    name: "Centre The Message Cpp",              category: "cpp",   description: "", link: "https://mbktechstudio.com/CentreTheMessageCpp/"    },
+        { imageURL: "https://project.mbktechstudio.com/Assets/Images/download.svg",  name: "Unity Feedback Report System",        category: "unity", description: "", link: "https://docs.mbktechstudio.com/quiz-game-cpp-cli/" },
+        { imageURL: "https://project.mbktechstudio.com/Assets/Images/cpp.png",       name: "Cpp Quiz Game",                       category: "cpp",   description: "", link: "https://docs.mbktechstudio.com/quiz-game-cpp-cli/" },
+        { imageURL: "https://project.mbktechstudio.com/Assets/Images/mainpage.png",  name: "MBK Tech Studio Website",             category: "web",   description: "", link: "https://docs.mbktechstudio.com/mbktechstudio.com/" },       
+    ];
+            
+    const productsContainer = document.querySelector('.products');
+    const searchInput = document.getElementById('searchProduct');
     const categoryFilter = document.getElementById('categoryFilter');
-    if (hash) {
-        // Set the dropdown to the hash value
-        categoryFilter.value = hash;
-        filterProducts();
-    }
-});
-        
-        
-        const products = [
-         
-            { imageURL: "https://project.mbktechstudio.com/Assets/Images/CTMCpp.png",    name: "Centre The Message Cpp",              category: "cpp",   description: "", link: "https://mbktechstudio.com/ProjectLink" },
-            { imageURL: "https://project.mbktechstudio.com/Assets/Images/download.svg",  name: "Project2",                            category: "game",  description: "", link: "https://mbktechstudio.com/ProjectLink" },
-            { imageURL: "https://project.mbktechstudio.com/Assets/Images/download.svg",  name: "Unity Feedback Report System",        category: "unity", description: "", link: "https://mbktechstudio.com/ProjectLink" },
-            { imageURL: "https://project.mbktechstudio.com/Assets/Images/cpp.png",       name: "Cpp Quiz Game",                       category: "cpp",   description: "", link: "https://docs.mbktechstudio.com/quiz-game-cpp-cli/" },
-            { imageURL: "https://project.mbktechstudio.com/Assets/Images/download.svg",  name: "MBK Tech Studio Website Source Code", category: "web",   description: "", link: "https://docs.mbktechstudio.com/mbktechstudio.com/" },
-            { imageURL: "https://project.mbktechstudio.com/Assets/Images/download.svg",  name: "Project6",                            category: "unity", description: "", link: "https://mbktechstudio.com/ProjectLink" },
             
-            ];
-            
-            const productsContainer = document.querySelector('.products');
-            const searchInput = document.getElementById('searchProduct');
-            const categoryFilter = document.getElementById('categoryFilter');
-            
-            function displayProducts(productsArray) {
-            productsContainer.innerHTML = "";
-            productsArray.forEach(product => {
+    function displayProducts(productsArray) {
+        productsContainer.innerHTML = "";
+        if (productsArray.length === 0) {
+            productsContainer.innerHTML = '<p class="nmessage">Project not found</p>';
+            return;
+        }
+        productsArray.forEach(product => {
             const productElement = document.createElement('div');
             productElement.classList.add('product');
             productElement.innerHTML = `
@@ -38,30 +38,29 @@ document.addEventListener("DOMContentLoaded", function() {
             `;
             //<p>Price: $${product.price}</p>
             productElement.addEventListener('click', () => {
-                window.location.href = product.link;
+                window.open(product.link, '_blank');
             });
-            productsContainer.appendChild(productElement)
-            });
-            }
+        productsContainer.appendChild(productElement)
+        });
+    }
             
-            function filterProducts() {
-            const selectedCategory = categoryFilter.value;
-            let filteredProducts = products;
-            if (selectedCategory !== 'all') {
+    function filterProducts() {
+        const selectedCategory = categoryFilter.value;
+        let filteredProducts = products;
+        if (selectedCategory !== 'all') {
             filteredProducts = products.filter(product => product.category === selectedCategory);
-            }
-            filteredProducts = searchProducts(filteredProducts);
-            displayProducts(filteredProducts);
-            }
+        }
+        filteredProducts = searchProducts(filteredProducts);
+        displayProducts(filteredProducts);
+    }
             
-            function searchProducts(productsArray) {
-            const searchText = searchInput.value.toLowerCase();
-            return productsArray.filter(product => product.name.toLowerCase().includes(searchText));
-            }
+    function searchProducts(productsArray) {
+        const searchText = searchInput.value.toLowerCase();
+        return productsArray.filter(product => product.name.toLowerCase().includes(searchText));
+    }
             
-            categoryFilter.addEventListener('change', filterProducts);
-            searchInput.addEventListener('input', filterProducts);
+    categoryFilter.addEventListener('change', filterProducts);
+    searchInput.addEventListener('input', filterProducts);
             
-            // Initial display of all products
-            displayProducts(products);
-            
+    // Initial display of all products
+    displayProducts(products);
